946275954dd8b94002689b819455e1a660174777
dangiansu <ansud.itpathsolutions@gmail.com>
2026-05-13 16:05:13 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3005;
+
+app.use(express.json());
+
+// Health Route
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    message: 'Server is healthy',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime()
+  });
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log("CI-CD with Docker and EC2 is working now...")
+});
